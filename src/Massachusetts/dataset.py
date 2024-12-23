47c90409fa38abf619893b0c2bcd00a821b95124
KrKOo <krkoo.123@gmail.com>
2024-12-23 19:24:00 +0000

    Add Segformer and DeepLabV4+

--- src/Massachusetts/dataset.py
+++ src/Massachusetts/dataset.py
@@ -4,17 +4,6 @@ import cv2
 import numpy as np
 
 def one_hot_encode(label, label_values):
-    """
-    Convert a segmentation image label array to one-hot format
-    by replacing each pixel value with a vector of length num_classes
-    # Arguments
-        label: The 2D array segmentation image label
-        label_values
-        
-    # Returns
-        A 2D array with the same width and hieght as the input, but
-        with a depth size of num_classes
-    """
     semantic_map = []
     for colour in label_values:
         equality = np.equal(label, colour)
--- src/Massachusetts/models.py
+++ src/Massachusetts/models.py
@@ -2,14 +2,16 @@ import os
 import segmentation_models_pytorch as smp
 from segmentation_models_pytorch.utils.train import TrainEpoch, ValidEpoch
 from segmentation_models_pytorch.utils.losses import DiceLoss
-from segmentation_models_pytorch.utils.metrics import IoU, Fscore
+from segmentation_models_pytorch.utils.metrics import IoU, Fscore, Precision, Recall
 
 import torch
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "CPU")
 METRICS = [
     IoU(threshold=0.5),
-    Fscore()
+#    Fscore(),
+#    Precision(),
+#    Recall()
 ]
 CLASSES = ["background", "road"]
 
@@ -58,5 +60,95 @@ class Unet():
 
         return train_epoch, valid_epoch
 
+
+class Segformer:
+    def __init__(self):
+        encoder = 'resnet50'
+        encoder_weights = 'imagenet'
+        activation = 'sigmoid'
+        
+        self.model = smp.Segformer(
+            encoder_name=encoder, 
+            encoder_weights=encoder_weights, 
+            classes=len(CLASSES), 
+            activation=activation,
+        )
+        self.preprocessing_fn = smp.encoders.get_preprocessing_fn(encoder, encoder_weights)
+        self.loss = DiceLoss()
+        
+        self.optimizer = torch.optim.Adam([ 
+            dict(params=self.model.parameters(), lr=0.0008),
+        ])
+        
+    def load_model(self, path):
+        self.model = torch.load(path, map_location=DEVICE)
+
+    def save_model(self, path):
+        torch.save(self.model, path)
+
+    def get_train_valid_epoch(self):
+        train_epoch = TrainEpoch(
+            self.model, 
+            loss=self.loss, 
+            metrics=METRICS, 
+            optimizer=self.optimizer,
+            device=DEVICE,
+            verbose=True,
+        )
+        
+        valid_epoch = ValidEpoch(
+            self.model, 
+            loss=self.loss, 
+            metrics=METRICS, 
+            device=DEVICE,
+            verbose=True,
+        )
+
+        return train_epoch, valid_epoch
+
+class DeepLabV3Plus:
+    def __init__(self):
+        encoder = 'resnet50'
+        encoder_weights = 'imagenet'
+        activation = 'sigmoid'
+        
+        self.model = smp.DeepLabV3Plus(
+            encoder_name=encoder, 
+            encoder_weights=encoder_weights, 
+            classes=len(CLASSES), 
+            activation=activation,
+        )
+        self.preprocessing_fn = smp.encoders.get_preprocessing_fn(encoder, encoder_weights)
+        self.loss = DiceLoss()
+        
+        self.optimizer = torch.optim.Adam([ 
+            dict(params=self.model.parameters(), lr=0.00008),
+        ])
+        
+    def load_model(self, path):
+        self.model = torch.load(path, map_location=DEVICE)
+
+    def save_model(self, path):
+        torch.save(self.model, path)
+
+    def get_train_valid_epoch(self):
+        train_epoch = TrainEpoch(
+            self.model, 
+            loss=self.loss, 
+            metrics=METRICS, 
+            optimizer=self.optimizer,
+            device=DEVICE,
+            verbose=True,
+        )
+        
+        valid_epoch = ValidEpoch(
+            self.model, 
+            loss=self.loss, 
+            metrics=METRICS, 
+            device=DEVICE,
+            verbose=True,
+        )
+
+        return train_epoch, valid_epoch
 
 
